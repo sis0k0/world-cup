387fdedf0b6c0d885f2f9cf2762b0e4df88a9d0c
sis0k0 <stanimira.vlaeva@gmail.com>
2018-07-06 19:29:21 +0300

    feat: display games

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { GameService } from './game.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'webapp';
+  games = [];
+
+  constructor(private gameService: GameService) {
+    this.gameService.getAll().subscribe((games) => {
+      this.games = games;
+    });
+  }
+
+  getScoreString(game) {
+    const { team1, team2, score1, score2 } = game;
+    return `${team1} ${score1} : ${score2} ${team2}`;
+  }
 }
