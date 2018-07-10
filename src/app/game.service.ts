f39b0b1010aecb3c175306ee93faedf60c0730f8
sis0k0 <stanimira.vlaeva@gmail.com>
2018-07-10 05:57:52 +0200

    refactor: add mocked games

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -4,6 +4,7 @@ import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { Game } from './game';
+// const games = require("./games.json");
 
 @Injectable({
   providedIn: 'root'
@@ -12,82 +13,38 @@ export class GameService {
   constructor(private httpClient: HttpClient) { }
 
   getAll(): Observable<Game[]> {
+    // return of(games)
     return this.httpClient.get<any>('https://worldcup.sfg.io/matches')
-      .pipe(map(
-        apiGames => apiGames
-        .map(game => {
-          const started = game.status !== 'future';
-          const isGoal = event => ['goal', 'goal-penalty'].includes(event.type_of_event);
-          const filterGoals = events => events.filter(isGoal);
-          const addTeam = (goal, team) => Object.assign(goal, { team });
-          const goals = started ?
-            [
-              ...filterGoals(game.home_team_events).map(goal => addTeam(goal, 'home')),
-              ...filterGoals(game.away_team_events).map(goal => addTeam(goal, 'away')),
-            ]
-              .sort((a, b) => parseInt(a.time) - parseInt(b.time)) :
-            [];
+      .pipe(
+        map(apiGames => {
+          return apiGames
+            .map(game => {
+              const started = game.status !== 'future';
+              const isGoal = event => ['goal', 'goal-penalty'].includes(event.type_of_event);
+              const filterGoals = events => events.filter(isGoal);
+              const addTeam = (goal, team) => Object.assign(goal, { team });
+              const goals = started ?
+                [
+                  ...filterGoals(game.home_team_events).map(goal => addTeam(goal, 'home')),
+                  ...filterGoals(game.away_team_events).map(goal => addTeam(goal, 'away')),
+                ]
+                  .sort((a, b) => parseInt(a.time) - parseInt(b.time)) :
+                [];
 
-          const { home_team, away_team } = game;
+              const { home_team, away_team } = game;
 
-          return Object.assign({}, {
-            team1: home_team.country,
-            team2: away_team.country,
-            score1: started ? home_team.goals : '-',
-            score2: started ? away_team.goals : '-',
-            goals,
-          });
-        })
-        .reverse()
+              return Object.assign({}, {
+                team1: home_team.country,
+                team2: away_team.country,
+                score1: started ? home_team.goals : '-',
+                score2: started ? away_team.goals : '-',
+                goals,
+              });
+            })
+            .reverse()
+          }
       ));
   }
 }
 
 
-// const games = [
-//     {
-//       id: 1,
-//       team1: 'England',
-//       team2: 'France',
-//       score1: 3,
-//       score2: 0,
-//       goals: [
-//         {
-//           player: "Harry Kane",
-//           time: 15,
-//           teamId: 1,
-//         },
-//         {
-//           player: "Danny Welbeck",
-//           time: 46,
-//           teamId: 1,
-//         },
-//         {
-//           player: "James Milner",
-//           time: 73,
-//           teamId: 1,
-//         },
-//       ],
-//     },
-// 
-//     {
-//       id: 2,
-//       team1: 'Argentina',
-//       team2: 'Brazil',
-//       score1: 1,
-//       score2: 1,
-//       goals: [
-//         {
-//           player: 'Leo Messi',
-//           time: 13,
-//           teamId: 0,
-//         },
-//         {
-//           player: 'Neymar',
-//           time: 86,
-//           teamId: 1,
-//         },
-//       ]
-//     },
-// ];
-// 
